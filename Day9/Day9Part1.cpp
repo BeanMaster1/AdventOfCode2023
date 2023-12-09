2148e18d01b2fa7df6e997964b5039f4df37a103
Aaron Kelly <aaronchristiankelly@gmail.com>
2023-12-08 23:25:03 -0600

    Day 9 Commit

---
+++ Day9/Day9Part1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+bool is_zeroes(vector<int> v)
+{
+    for(int i = 0; i < v.size(); i++)
+    {
+        if(v[i]!=0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    fstream finput("Day9Input.txt");
+    string buf;
+    int sum = 0;
+    while(getline(finput, buf))
+    {
+        istringstream line(buf);
+        vector<vector<int>> sequences;
+        vector<int> first_sequence;
+        int num_buf;
+        while(line >> num_buf)
+        {
+            first_sequence.push_back(num_buf);
+        }
+        sequences.push_back(first_sequence);
+        for(int i = 0; !is_zeroes(sequences[i]); i++)
+        {
+            vector<int> next_sequence;
+            for(int j = 1; j < sequences[i].size(); j++)
+            {
+                next_sequence.push_back(sequences[i][j] - sequences[i][j-1]);
+            }
+            sequences.push_back(next_sequence);
+        }
+        sequences[sequences.size()-1].push_back(0);
+        for(int i = sequences.size()-2; i >= 0; i--)
+        {
+            sequences[i].push_back(sequences[i][sequences[i].size()-1] + sequences[i+1][sequences[i].size()-1]);
+        }
+        sum += sequences[0][sequences[0].size()-1];
+    }
+    cout << sum;
+}
